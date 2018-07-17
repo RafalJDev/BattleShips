c1796bc8b6df9fbd288e6f7cead670942e06f95a
Jakub Gacik <Jakub_Gacik@epam.com>
2018-07-17 14:37:09 +0200

    Added basic model for Game class

---
+++ backend/src/main/java/pl/krkteam/battleships/common/domain/Game.java
@@ -0,0 +1,12 @@
+package pl.krkteam.battleships.common.domain;
+
+import pl.krkteam.battleships.opponent_shot_result_keeping.ShotResultQueueHolder;
+
+public class Game {
+
+    private GameBoardHolder gameBoardHolder;
+
+    private ShotResultQueueHolder shotResultQueueHolder;
+
+
+}
