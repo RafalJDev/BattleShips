6673e6deba9ea5a51fa7d665187c9cf9d6ed6148
Jakub Gacik <Jakub_Gacik@epam.com>
2018-07-23 15:33:29 +0200

    Added code to model classes

--- backend/src/main/java/pl/krkteam/battleships/turns/holding/TurnController.java
+++ backend/src/main/java/pl/krkteam/battleships/turns/holding/TurnController.java
@@ -1,4 +1,26 @@
 package pl.krkteam.battleships.turns.holding;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.krkteam.battleships.common.domain.player.Player;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
 public class TurnController {
+
+    @Autowired
+    private TurnHolder turnHolder;
+
+    @GetMapping(value = "/game/player/isTurn")
+    public String isTurnOfPlayer(@RequestParam String playerName) {
+        Player player = new Player(playerName);
+        boolean result = turnHolder.isTurnOfPlayer(player);
+        String send = "{\"message\":" + "\"" + result + "\"" + " }";
+        return send;
+    }
+    
+
 }
--- backend/src/main/java/pl/krkteam/battleships/turns/holding/TurnHolder.java
+++ backend/src/main/java/pl/krkteam/battleships/turns/holding/TurnHolder.java
@@ -1,4 +1,69 @@
 package pl.krkteam.battleships.turns.holding;
 
+import org.springframework.stereotype.Component;
+import pl.krkteam.battleships.common.domain.player.Player;
+import pl.krkteam.battleships.shooting.dto.result.ShotResultDTO;
+
+import java.util.*;
+
+@Component
 public class TurnHolder {
+
+    volatile private Player currentPlayer;
+
+    private final Player firstPlayer;
+
+    private final Player secondPlayer;
+
+    private volatile boolean gameHasBeenStarted = false;
+
+    public TurnHolder(Player initialPlayer, Player opponent) {
+        if (initialPlayer == null || opponent == null) {
+            throw new IllegalArgumentException("Player cannot be null");
+        }
+        firstPlayer = initialPlayer;
+        secondPlayer = opponent;
+        currentPlayer = initialPlayer;
+    }
+
+    public synchronized void choseFirst(Player pretendToBeFirst) {
+        if (gameHasBeenStarted) {
+            throw new IllegalStateException("First Player cannot be set - game already is in progress");
+        }
+        if (!pretendToBeFirst.equals(firstPlayer) && !pretendToBeFirst.equals(secondPlayer)) {
+            throw new NoSuchElementException("Player is not registered in this holder");
+        }
+        currentPlayer = pretendToBeFirst;
+    }
+
+    public synchronized void addShotResult(Player shootingPlayer, ShotResultDTO shotResult) {
+        if (shotResult == null) {
+            throw new IllegalArgumentException("ShotResult cannot be null");
+        }
+        if (!shootingPlayer.equals(firstPlayer) && !shootingPlayer.equals(secondPlayer)) {
+            throw new NoSuchElementException("Player is not registered in this holder");
+        }
+        switch (shotResult.getMessage()) {
+            case "ResultHit":
+            case "ResultSunk":
+                currentPlayer = shootingPlayer;
+                break;
+            case "ResultMiss":
+            case "WrongShot":
+                currentPlayer = shootingPlayer.equals(firstPlayer) ? secondPlayer : firstPlayer;
+        }
+        gameHasBeenStarted = true;
+    }
+
+    public synchronized Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public synchronized boolean isTurnOfPlayer(Player player) {
+        if (!player.equals(firstPlayer) && !player.equals(secondPlayer)) {
+            throw new NoSuchElementException("Player is not registered in this holder");
+        }
+        return currentPlayer.equals(player);
+    }
+
 }
