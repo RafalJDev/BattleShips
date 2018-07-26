1b80e70c3d4b11c400c493fd1481a48791f826d1
RafalJDev <jaszczynski.rafal@gmail.com>
2018-07-26 12:33:37 +0200

    Review changes: deleting comments

--- frontend/src/app/views/boards/player-board/player-board.component.ts
+++ frontend/src/app/views/boards/player-board/player-board.component.ts
@@ -1,9 +1,8 @@
-import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
-import {ShipGenerator} from "../../../services/ship-generator/ship-generator.service";
-import {BoardOfCells} from "../../../models/domain/board/board-of-cells";
-import {ShipArray} from "../../../models/domain/ship/ship-array";
-import {ShipSender} from "../../../rest/post/ship-sender";
-import {DragShipService} from "../../../services/drag-ship/drag-ship.service";
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core'
+import {ShipGenerator} from "../../../services/ship-generator/ship-generator.service"
+import {BoardOfCells} from "../../../models/domain/board/board-of-cells"
+import {ShipSender} from "../../../rest/post/ship-sender"
+import {DragShipService} from "../../../services/drag-ship/drag-ship.service"
 import {BoardTransferSingelton} from "../../fleet-placing/transfer-class/board-transfer-singelton"
 
 @Component({
@@ -40,34 +39,6 @@ export class PlayerBoardComponent implements OnInit {
 
     this.boardDivEmitter.emit(this.boardDiv);
   }
-
-  // generateRandomBoard() {
-  //   this.playerBoard = new BoardOfCells();
-  //   this.playerBoard.generateBoardWithWater(10);
-  //
-  //   this.putShipsFromShipGenerator_random();
-  // }
-  //
-  // putShipsFromShipGenerator_random() {
-  //   const shipArray = this.shipGenerator.generateShipsRandomly(this.playerBoard);
-  //
-  //   this.testShipSending(shipArray);
-  // }
-  //
-  // testShipSending(shipArray: ShipArray) {
-  //
-  //   const shipArrayJson = JSON.stringify(shipArray);
-  //
-  //   console.log(shipArrayJson);
-  //
-  //   this.shipSender.postShip(shipArrayJson)
-  //     .then(result => {
-  //       this.result2 = result['result'];
-  //       if (this.result2 == 'true') {
-  //         console.log("Emiting result");
-  //       }
-  //     });
-  // }
 
   takeJustBoardDiv(boardDiv: Element) {
     console.log("clickOnPlayerBoar");
