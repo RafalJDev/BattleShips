13e6c7a98a2fcb00523012cfc8db1cbde6ba28b4
Krzysiek <krzysztof_sroczyk@epam.com>
2018-07-31 15:53:30 +0200

    sleep on remove room

--- backend/src/main/java/pl/krkteam/battleships/shooting/controller/ShootingController.java
+++ backend/src/main/java/pl/krkteam/battleships/shooting/controller/ShootingController.java
@@ -60,9 +60,16 @@ public class ShootingController {
     }
 
     private void onGameEnd(ShotResultDTO shotResultDTO, String roomName) {
-        if (shotResultDTO instanceof ResultPlayerWonDTO) {
-            roomHolder.removeRoom(roomName);
-        }
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (shotResultDTO instanceof ResultPlayerWonDTO) {
+                roomHolder.removeRoom(roomName);
+            }
+        }).start();
     }
 
     private Game getGameFromRoom(String roomName) {
